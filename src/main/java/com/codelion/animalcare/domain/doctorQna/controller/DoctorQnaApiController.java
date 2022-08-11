8949ae3a10968067fcd5ebdb65d6d1de9632c9de
Shin-sangwon <dnjsml10@gmail.com>
2022-08-11 22:41:19 +0900

    feat : DoctorQnaApiController 추가

---
+++ src/main/java/com/codelion/animalcare/domain/doctorQna/controller/DoctorQnaApiController.java
@@ -0,0 +1,9 @@
+package com.codelion.animalcare.domain.doctorQna.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class DoctorQnaApiController {
+}
