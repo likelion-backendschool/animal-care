e8fd0b5369abe9af8c6cad0093f7c518478e4214
timssuh@naver.comgit config --global user.namegit config --global user.email timssuh@naver.com <timssuh@naver.com>
2022-08-16 00:43:21 +0900

    feat: Add member entity and repository

--- src/main/java/com/codelion/animalcare/domain/member/entity/Member.java
+++ src/main/java/com/codelion/animalcare/domain/member/entity/Member.java
@@ -1,4 +1,4 @@
-package com.codelion.animalcare.domain.doctor.entity;
+package com.codelion.animalcare.domain.member.entity;
 
 import com.codelion.animalcare.common.entity.BaseEntity;
 import lombok.AccessLevel;
@@ -8,13 +8,12 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Doctor extends BaseEntity {
+public class Member extends BaseEntity {
     @Column(nullable = false, length = 50, unique = true)
     private String loginEmail;
 
@@ -27,28 +26,24 @@ public class Doctor extends BaseEntity {
     @Column()
     private LocalDateTime birthday;
 
-    @Column(nullable = false, length = 30)
-    private String major;
+    @Column(nullable = false, length = 70)
+    private String address;
 
     @Column(nullable = false, length = 20)
     private String phoneNum;
-
-    @Column(columnDefinition = "TEXT")
-    private String introduce;
 
     @Column()
     private LocalDateTime deletedAt;
 
     @Builder
-    private Doctor(Long id, LocalDateTime createdAt, String loginEmail, String loginPwd, String name, LocalDateTime birthday, String major, String phoneNum, String introduce, LocalDateTime deletedAt) {
+    private Member(Long id, LocalDateTime createdAt, String loginEmail, String loginPwd, String name, LocalDateTime birthday, String address, String phoneNum, LocalDateTime deletedAt) {
         super(id, createdAt);
         this.loginEmail = loginEmail;
         this.loginPwd = loginPwd;
         this.name = name;
         this.birthday = birthday;
-        this.major = major;
+        this.address = address;
         this.phoneNum = phoneNum;
-        this.introduce = introduce;
         this.deletedAt = deletedAt;
     }
 }
