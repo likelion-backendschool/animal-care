6ad0be7decbb8df66ba8023e2b8d73f67aa78f11
Shin-sangwon <dnjsml10@gmail.com>
2022-08-15 21:17:04 +0900

    feat : Answer 등록 구현

--- src/main/java/com/codelion/animalcare/domain/doctorQna/controller/AnswerController.java
+++ src/main/java/com/codelion/animalcare/domain/doctorQna/controller/AnswerController.java
@@ -1,4 +1,25 @@
 package com.codelion.animalcare.domain.doctorQna.controller;
 
+
+import com.codelion.animalcare.domain.doctorQna.controller.dto.request.AnswerSaveRequestDto;
+import com.codelion.animalcare.domain.doctorQna.service.AnswerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
 public class AnswerController {
+
+    private final AnswerService answerService;
+
+    //답변 작성
+    @PostMapping("/usr/doctor-qna/{id}/answers/write")
+    public Long save(@PathVariable Long id, @RequestBody AnswerSaveRequestDto answerSaveRequestDto){
+
+        return answerService.save(id, answerSaveRequestDto);
+    }
+
 }
---
+++ src/main/java/com/codelion/animalcare/domain/doctorQna/service/AnswerService.java
@@ -0,0 +1,25 @@
+package com.codelion.animalcare.domain.doctorQna.service;
+
+import com.codelion.animalcare.domain.doctorQna.controller.dto.request.AnswerSaveRequestDto;
+import com.codelion.animalcare.domain.doctorQna.repository.AnswerRepository;
+import com.codelion.animalcare.domain.doctorQna.repository.Question;
+import com.codelion.animalcare.domain.doctorQna.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+
+    private final AnswerRepository answerRepository;
+    private final QuestionRepository questionRepository;
+
+    @Transactional
+    public Long save(Long id, AnswerSaveRequestDto answerSaveRequestDto){
+        Question question = questionRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("질문이 존재하지 않습니다."));
+
+
+        return answerRepository.save(answerSaveRequestDto.toEntity()).getAnswerId();
+    }
+}
