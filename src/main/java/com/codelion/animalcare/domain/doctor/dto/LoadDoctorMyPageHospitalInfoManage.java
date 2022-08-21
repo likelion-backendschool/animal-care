9c804a7d67c97463af0a717f12e889df79c69ce8
timssuh@naver.comgit config --global user.namegit config --global user.email timssuh@naver.com <timssuh@naver.com>
2022-08-21 23:04:13 +0900

    ìfix: Modify the url of templates & Add getter in LoadDoctorMyPageHospitalInfoManage.ResponseDto

--- src/main/java/com/codelion/animalcare/domain/doctor/dto/LoadDoctorMyPageHospitalInfoManage.java
+++ src/main/java/com/codelion/animalcare/domain/doctor/dto/LoadDoctorMyPageHospitalInfoManage.java
@@ -2,10 +2,12 @@ package com.codelion.animalcare.domain.doctor.dto;
 
 import com.codelion.animalcare.domain.hospital.entity.Hospital;
 import com.codelion.animalcare.domain.member.Address;
+import lombok.Getter;
 
 import javax.persistence.Embedded;
 
 public class LoadDoctorMyPageHospitalInfoManage {
+    @Getter
     public static class ResponseDto{
         private String name;
 
