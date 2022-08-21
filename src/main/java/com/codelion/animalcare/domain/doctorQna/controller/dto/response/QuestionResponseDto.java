8af4ea10a2b11e7ea247701cd153913520627e8d
Shin-sangwon <dnjsml10@gmail.com>
2022-08-21 13:20:30 +0900

    feat : doctorqna 개별조회 타임리프 적용

--- src/main/java/com/codelion/animalcare/domain/doctorQna/controller/dto/response/QuestionResponseDto.java
+++ src/main/java/com/codelion/animalcare/domain/doctorQna/controller/dto/response/QuestionResponseDto.java
@@ -4,6 +4,7 @@ import com.codelion.animalcare.domain.doctorQna.repository.Answer;
 import com.codelion.animalcare.domain.doctorQna.repository.Question;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -12,6 +13,8 @@ public class QuestionResponseDto {
     private Long id;
     private String title;
     private String content;
+
+    private LocalDateTime createdAt;
     private int view;
 
     private List<Answer> answerList;
@@ -22,6 +25,7 @@ public class QuestionResponseDto {
         this.title = entity.getTitle();
         this.content = entity.getContent();
         this.view = entity.getView();
+        this.createdAt = entity.getCreatedAt();
         this.answerList = getAnswerList();
     }
 }
