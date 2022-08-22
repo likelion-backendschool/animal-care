50733d90054db1051a58f11853abf380e3232ff1
KCH <doolysmile1@naver.com>
2022-08-23 02:15:48 +0900

    chore : security 의존성 추가, Oath2 의존성 추가, H2 의존성 삭제

---
+++ src/main/java/com/codelion/animalcare/domain/user/domain/UserInfoDto.java
@@ -0,0 +1,13 @@
+package com.codelion.animalcare.user.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserInfoDto {
+    private String email;
+    private String password;
+
+    private String auth;
+}
