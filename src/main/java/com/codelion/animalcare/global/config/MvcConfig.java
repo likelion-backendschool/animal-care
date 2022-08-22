8549966fac2a22cafd1df96e931ea5e742656f67
KCH <doolysmile1@naver.com>
2022-08-23 02:18:21 +0900

    feat : 웹 관련 config파일 추가

---
+++ src/main/java/com/codelion/animalcare/global/config/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.codelion.animalcare.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    // 요청 - 뷰 연결
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("login/main");
+        registry.addViewController("/login").setViewName("login/login");
+        registry.addViewController("/admin").setViewName("login/admin");
+        registry.addViewController("/signup").setViewName("login/signup");
+        registry.addViewController("/test").setViewName("test");
+    }
+}
