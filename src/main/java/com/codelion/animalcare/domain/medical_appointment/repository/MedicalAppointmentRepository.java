3df39d918c15096c41aecb64342c7f4aad1c07e0
brightGarden02 <rreone2017@gmail.com>
2022-08-25 10:29:07 +0900

    feat: MedicalAppointmentRepository에 어노테이션 Repository 제거

--- src/main/java/com/codelion/animalcare/domain/medical_appointment/repository/MedicalAppointmentRepository.java
+++ src/main/java/com/codelion/animalcare/domain/medical_appointment/repository/MedicalAppointmentRepository.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 
-@Repository
 public interface MedicalAppointmentRepository extends JpaRepository<MedicalAppointment, Long> {
 
 
