74387cbc7c09e982d7666bc461cd4fd50644aefe
brightGarden02 <rreone2017@gmail.com>
2022-08-31 10:11:52 +0900

    feat: Address클래스에 @NoArgsConstructor lombok처리

--- src/main/java/com/codelion/animalcare/domain/user/entity/Address.java
+++ src/main/java/com/codelion/animalcare/domain/user/entity/Address.java
@@ -1,19 +1,20 @@
 package com.codelion.animalcare.domain.user.entity;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
 
-    protected Address() {
-    }
 
     public Address(String city, String street, String zipcode) {
         this.city = city;
