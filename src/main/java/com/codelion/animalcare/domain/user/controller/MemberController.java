b60e68b761a27c2b039f31472f90810e7b231d61
KCH <doolysmile1@naver.com>
2022-09-06 21:27:19 +0900

    feat : 멤버 로그인으로 분리

--- src/main/java/com/codelion/animalcare/domain/user/controller/MemberController.java
+++ src/main/java/com/codelion/animalcare/domain/user/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.codelion.animalcare.domain.user.controller;
 
+import com.codelion.animalcare.domain.user.dto.DoctorSignUpDto;
 import com.codelion.animalcare.domain.user.dto.MemberDto;
+import com.codelion.animalcare.domain.user.dto.MemberSignUpDto;
 import com.codelion.animalcare.domain.user.entity.Address;
 import com.codelion.animalcare.domain.user.entity.Member;
 import com.codelion.animalcare.domain.user.entity.MemberForm;
@@ -62,7 +64,14 @@ public class MemberController {
     }
 
     @GetMapping("/usr/member/signup")
-    public String signup(MemberDto memberDto){
+    public String signup(Model model){
+        model.addAttribute("memberSignUpDto", new MemberSignUpDto());
         return "login/memberSignup";
+    }
+
+    @PostMapping("/usr/member/signup")
+    public String signup(MemberSignUpDto memberSignUpDto){
+        memberService.save(memberSignUpDto);
+        return "main";
     }
 }
---
+++ src/main/java/com/codelion/animalcare/domain/user/dto/MemberSignUpDto.java
@@ -0,0 +1,56 @@
+package com.codelion.animalcare.domain.user.dto;
+
+import com.codelion.animalcare.domain.hospital.entity.Hospital;
+import com.codelion.animalcare.domain.user.entity.Address;
+import com.codelion.animalcare.domain.user.entity.Doctor;
+import com.codelion.animalcare.domain.user.entity.Member;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MemberSignUpDto {
+
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private Date birthDay;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    private String phoneNum;
+
+    private Integer genderId;
+
+    private String auth;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+
+    public Member toEntity(MemberSignUpDto memberSignUpDto){
+        return Member.builder()
+                .email(memberSignUpDto.getEmail())
+                .password(memberSignUpDto.getPassword())
+                .name(memberSignUpDto.getName())
+                .birthday((java.sql.Date) memberSignUpDto.getBirthDay())
+                .address(new Address(memberSignUpDto.getCity(), memberSignUpDto.getStreet(), memberSignUpDto.getZipcode()))
+                .phoneNum(memberSignUpDto.getPhoneNum())
+                .genderId(memberSignUpDto.getGenderId())
+                .auth("ROLE_MEMBER")
+                .build();
+    }
+}
