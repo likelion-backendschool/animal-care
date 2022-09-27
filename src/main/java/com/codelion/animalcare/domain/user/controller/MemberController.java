13a655b747210a417a2765a9f2dc2a72427ea6e0
KCH <doolysmile1@naver.com>
2022-09-27 09:56:23 +0900

    chore : import 추가

--- src/main/java/com/codelion/animalcare/domain/user/controller/MemberController.java
+++ src/main/java/com/codelion/animalcare/domain/user/controller/MemberController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 import java.security.Principal;
