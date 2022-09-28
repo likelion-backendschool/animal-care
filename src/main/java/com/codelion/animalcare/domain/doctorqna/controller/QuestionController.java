4de49bba49ec25b0ff03cb446d8806ad88fa09a2
Shin-sangwon <dnjsml10@gmail.com>
2022-09-28 10:22:13 +0900

    style : 주석 일부 삭제

--- src/main/java/com/codelion/animalcare/domain/doctorqna/controller/QuestionController.java
+++ src/main/java/com/codelion/animalcare/domain/doctorqna/controller/QuestionController.java
@@ -25,12 +25,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.security.Principal;
 
-// 질문 등록, 목록에 표현, 답변 갯수 표시, 답변 등록, 답변 표시, 수정 삭제 (임시)프론트, 로그인 연계 기능들(질문 삭제, 수정, 답변 삭제, 수정) , VALID 기능으로 폼에 입력 제한 걸기
-// 구현 완료
-/* TODO : 조회수 구현하기, 해시태그로 게시물 표시하기
-    프론트 부트스트랩 적용, 질문 답변 작성자 표시기능, 질문 답변 좋아요 기능, 수정시간 > 입력시간? 수정시간 : 입력시간 구현하기
-    페이징(우용님꺼랑 같게), 앵커, 추천, 검색
-*/
 
 @RequiredArgsConstructor
 @Controller
