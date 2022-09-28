45b50cd9645389f83d4971b9c62b18ea67fb7d95
KCH <doolysmile1@naver.com>
2022-09-28 16:12:07 +0900

    refactor : webRTC 수정

--- src/main/java/com/codelion/animalcare/webrtc/config/WebSocketConfig.java
+++ src/main/java/com/codelion/animalcare/webrtc/config/WebSocketConfig.java
@@ -14,7 +14,8 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(signalHandler(), "/signal")
-                .setAllowedOrigins("*"); // allow all origins
+                .setAllowedOrigins("https://public.animal-care.site/");
+//                .setAllowedOrigins("*"); // allow all origins
     }
 
     @Bean
