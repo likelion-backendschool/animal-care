da1d6f7f6a4c143f731dd3fbf872ccafd623395a
brightGarden02 <rreone2017@gmail.com>
2022-09-29 13:18:29 +0900

    refactor: sql 변수명 변경

--- src/main/java/com/codelion/animalcare/domain/appointment/repository/AppointmentRepository.java
+++ src/main/java/com/codelion/animalcare/domain/appointment/repository/AppointmentRepository.java
@@ -17,21 +17,21 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Long> 
      * 1. 비대면 진료시 Doctor가 예약내역 조회
      * 2. DoctorMyPage 환자 예약  관리
      */
-    @Query("select ma from Appointment ma join fetch  ma.member m join fetch  ma.animal a join fetch ma.doctor d join fetch ma.hospital h where d.id = :doctorId ")
+    @Query("select ap from Appointment ap join fetch  ap.member m join fetch  ap.animal a join fetch ap.doctor d join fetch ap.hospital h where d.id = :doctorId ")
     List<Appointment> findByDoctorId(@Param("doctorId") long doctorId);
 
     /**
      * Member가 MemberMyPage에서 예약내역 조회
      */
-    @Query("select ma from Appointment ma join fetch  ma.member m join fetch  ma.animal a join fetch ma.doctor d join fetch ma.hospital h where m.id = :memberId")
+    @Query("select ap from Appointment ap join fetch  ap.member m join fetch  ap.animal a join fetch ap.doctor d join fetch ap.hospital h where m.id = :memberId")
     List<Appointment> findByMemberId(@Param("memberId") long memberId);
 
 
-    @Query("select ma from Appointment ma join fetch  ma.member m join fetch  ma.animal a join fetch ma.doctor d join fetch ma.hospital h where ma.id = :appointmentId")
+    @Query("select ap from Appointment ap join fetch  ap.member m join fetch  ap.animal a join fetch ap.doctor d join fetch ap.hospital h where ap.id = :appointmentId")
     Optional<Appointment> findByAppointmentId(@Param("appointmentId") long appointmentId);
 
 
-    @Query("select ma.date from Appointment ma where ma.doctor.id = :doctorId and :utcDateTimeFront <= ma.date and ma.date < :utcDateTimeEnd")
+    @Query("select ap.date from Appointment ap where ap.doctor.id = :doctorId and :utcDateTimeFront <= ap.date and ap.date < :utcDateTimeEnd")
     List<LocalDateTime> findDateTimesByDateAndDoctor(
             @Param("utcDateTimeFront")LocalDateTime utcDateTimeFront,
             @Param("utcDateTimeEnd")LocalDateTime utcDateTimeEnd,
