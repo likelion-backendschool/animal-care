e3561402fa4dc33fc3ff0182df34c5280c69272c
brightGarden02 <rreone2017@gmail.com>
2022-11-04 16:41:52 +0900

    refactor: AnswerService 예외처리 메서드를 통해 중복코드 간소화

--- src/main/java/com/codelion/animalcare/domain/doctorqna/service/AnswerService.java
+++ src/main/java/com/codelion/animalcare/domain/doctorqna/service/AnswerService.java
@@ -21,7 +21,6 @@ public class AnswerService {
 
     private final AnswerRepository answerRepository;
     private final QuestionRepository questionRepository;
-
     private final DoctorRepository doctorRepository;
     private final QuestionService questionService;
 
@@ -44,37 +43,41 @@ public class AnswerService {
     public Long update(Long questionId, Long answerId, AnswerUpdateRequestDto answerUpdateRequestDto) {
 
         Question question = questionService.findQuestionByQuestionId(questionId);
-        Answer answer = answerRepository.findById(answerId).orElseThrow(() -> new IllegalArgumentException("답변이 존재하지 않습니다."));
+        Answer answer = findAnswerByAnswerId(answerId);
 
         answer.update(answerUpdateRequestDto.getContent());
 
         return answerId;
     }
 
+
     @Transactional
     public void delete(Long questionId, Long answerId) {
         Question question = questionService.findQuestionByQuestionId(questionId);
-        Answer answer = answerRepository.findById(answerId).orElseThrow(() -> new IllegalArgumentException("답변이 존재하지 않습니다."));
+        Answer answer = findAnswerByAnswerId(answerId);
 
         answerRepository.delete(answer);
     }
 
     @Transactional(readOnly = true)
-    public AnswerResponseDto findById(Long id){
-        Answer entity = answerRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("답변이 존재하지 않습니다."));
+    public AnswerResponseDto findById(Long answerId){
+        Answer entity = findAnswerByAnswerId(answerId);
 
         return new AnswerResponseDto(entity);
     }
 
 
     public boolean answerAuthorized(Long answerId, Principal principal) {
-        Answer answer = answerRepository.findById(answerId).orElseThrow(() -> new IllegalArgumentException("작성된 글이 없습니다."));
-
+        Answer answer = findAnswerByAnswerId(answerId);
 
         if(answer.getDoctor().getEmail().equals(principal.getName())) {
             return false;
         }
 
         return true;
+    }
+
+    public Answer findAnswerByAnswerId(Long answerId) {
+        return answerRepository.findById(answerId).orElseThrow(() -> new IllegalArgumentException("답변이 존재하지 않습니다."));
     }
 }
