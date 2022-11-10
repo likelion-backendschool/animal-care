aae4ef708d712509a4e6ebd8d099ba2e938dc34a
brightGarden02 <rreone2017@gmail.com>
2022-11-10 19:35:56 +0900

    feat: AppointmentQueryTest findAllAppointment()

---
+++ src/test/java/com/codelion/animalcare/domain/appointment/AppointmentQueryServiceTests.java
@@ -0,0 +1,83 @@
+package com.codelion.animalcare.domain.appointment;
+
+import com.codelion.animalcare.domain.appointment.dto.AppointmentDto;
+import com.codelion.animalcare.domain.appointment.entity.Appointment;
+import com.codelion.animalcare.domain.appointment.repository.AppointmentRepository;
+import com.codelion.animalcare.domain.appointment.service.AppointmentQueryService;
+import com.codelion.animalcare.domain.user.entity.Doctor;
+import com.codelion.animalcare.domain.user.repository.DoctorRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@Transactional
+@WebAppConfiguration
+public class AppointmentQueryServiceTests {
+
+    @Autowired
+    private AppointmentQueryService appointmentQueryService;
+
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+
+
+    @Test
+    void findAllAppointmentEntityListTest() throws Exception {
+
+        //given
+        Doctor doctor = doctorRepository.findById(5L).get();
+
+        //when
+        List<Appointment> appointments = appointmentRepository.findByDoctorId(doctor.getId());
+
+        //then
+        assertThat(appointments.size()).isEqualTo(9);
+    }
+
+
+    @Test
+    void findAllAppointmentDtoListTest() throws Exception {
+
+        //given
+        Doctor doctor = doctorRepository.findById(5L).get();
+
+        //when
+        List<Appointment> appointments = appointmentRepository.findByDoctorId(doctor.getId());
+        List<AppointmentDto> appointmentDtos = appointments
+                .stream()
+                .map(o -> new AppointmentDto(o))
+                .collect(Collectors.toList());
+
+        //then
+        assertThat(appointmentDtos.size()).isEqualTo(9);
+    }
+
+
+    @Test
+    void findAllAppointmentTest() throws Exception {
+
+        //given
+        Doctor doctor = doctorRepository.findById(5L).get();
+
+        //when
+        List<AppointmentDto> appointmentDtos = appointmentQueryService.findAllAppointment(doctor.getEmail());
+
+        //then
+        assertThat(appointmentDtos.size()).isEqualTo(9);
+    }
+
+
+
+}
