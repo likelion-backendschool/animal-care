4a728b68edc0f9f85142342845cf3979e869df26
Shin-sangwon <dnjsml10@gmail.com>
2023-02-06 23:16:25 +0900

    test : add answerCommandServiceTest

--- src/test/java/com/codelion/animalcare/doctorqna/service/AnswerCommandServiceTest.java
+++ src/test/java/com/codelion/animalcare/doctorqna/service/AnswerCommandServiceTest.java
@@ -1,11 +1,12 @@
 package com.codelion.animalcare.doctorqna.service;
 
 import com.codelion.animalcare.domain.doctorqna.dto.request.AnswerSaveRequestDto;
+import com.codelion.animalcare.domain.doctorqna.dto.request.AnswerUpdateRequestDto;
 import com.codelion.animalcare.domain.doctorqna.entity.Answer;
 import com.codelion.animalcare.domain.doctorqna.entity.Question;
 import com.codelion.animalcare.domain.doctorqna.repository.QuestionRepository;
 import com.codelion.animalcare.domain.doctorqna.service.AnswerCommandService;
-import com.codelion.animalcare.domain.user.repository.DoctorRepository;
+import com.codelion.animalcare.domain.doctorqna.service.AnswerQueryService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,7 @@ import java.security.Principal;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ActiveProfiles("test")
 @WebAppConfiguration
@@ -28,7 +30,7 @@ public class AnswerCommandServiceTest {
     @Autowired
     private AnswerCommandService answerCommandService;
     @Autowired
-    private DoctorRepository doctorRepository;
+    private AnswerQueryService answerQueryService;
     @Autowired
     private QuestionRepository questionRepository;
 
@@ -52,5 +54,33 @@ public class AnswerCommandServiceTest {
         assertEquals(answerList.size(), 2);
         assertEquals(testAnswer.getContent(), "열심히 공부하세요.");
 
+    }
+
+    @DisplayName("답변_수정된다")
+    @Test
+    void t2() {
+
+        //given
+        AnswerUpdateRequestDto answerUpdateRequestDto = AnswerUpdateRequestDto.builder()
+                .content("수정합니다")
+                .build();
+
+        //when
+        answerCommandService.update(1L, answerUpdateRequestDto);
+        Answer answer = answerQueryService.findAnswerByAnswerId(1L);
+        //then
+        assertEquals(answer.getContent(), "수정합니다");
+    }
+
+    @DisplayName("답변_삭제된다")
+    @Test
+    void t3() {
+
+        //given -> testinit
+
+        //when
+        answerCommandService.delete(1L);
+        //then
+        assertThrows(IllegalArgumentException.class, () -> answerQueryService.findAnswerByAnswerId(1L));
     }
 }
