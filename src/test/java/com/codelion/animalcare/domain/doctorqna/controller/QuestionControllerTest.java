14b3970685de8b9735f1afa08d5105bb17a6216b
Shin-sangwon <dnjsml10@gmail.com>
2023-04-02 12:04:20 +0900

    test : questionControllerTest 작성(이후 수정)

--- src/test/java/com/codelion/animalcare/domain/doctorqna/controller/QuestionControllerTest.java
+++ src/test/java/com/codelion/animalcare/domain/doctorqna/controller/QuestionControllerTest.java
@@ -1,29 +1,57 @@
 package com.codelion.animalcare.domain.doctorqna.controller;
 
+import com.codelion.animalcare.domain.appointment.interceptor.HasAnimalsInterceptor;
 import com.codelion.animalcare.domain.doctorqna.dto.request.QuestionSaveRequestDto;
 import com.codelion.animalcare.domain.doctorqna.service.QuestionCommandService;
+import com.codelion.animalcare.domain.doctorqna.service.QuestionQueryService;
+import com.codelion.animalcare.domain.user.service.UserService;
+import com.codelion.animalcare.global.config.MvcConfig;
+import com.codelion.animalcare.webrtc.controller.WebrtcController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.validation.BindingResult;
 
 import java.security.Principal;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
-import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@ActiveProfiles("test")
+@MockBean(JpaMetamodelMappingContext.class)
 @ExtendWith(MockitoExtension.class)
+@WebMvcTest(controllers = QuestionController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = MvcConfig.class),
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = HasAnimalsInterceptor.class),
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebrtcController.class)})
 public class QuestionControllerTest {
 
-    @Mock
+    @MockBean
     private QuestionCommandService questionCommandService;
+
+    @MockBean
+    private QuestionQueryService questionQueryService;
+
+    @MockBean
+    private UserService userService;
 
     @InjectMocks
     private QuestionController questionController;
@@ -34,33 +62,66 @@ public class QuestionControllerTest {
     @Mock
     private Principal principal;
 
-    @DisplayName("QuestionController_질문등록_성공")
+    @Autowired
+    private MockMvc mockMvc;
+
+    @DisplayName("권한이_없으면_Redirection_된다")
     @Test
-    void save_questionSaveRequestDtoIsValid_success() throws Exception {
+    void saveForm_Fail() throws Exception {
+
+        String viewName = questionController.saveForm(new QuestionSaveRequestDto("title", "content"));
+
+        mockMvc.perform(get("/usr/doctor-qna/write"))
+                .andExpect(status().is3xxRedirection());
+
+
+    }
+
+    @DisplayName("QuestionController_질문작성폼_보여진다")
+    @WithMockUser(roles="MEMBER")
+    @Test
+    void saveForm_Success() throws Exception {
+
+        String viewName = questionController.saveForm(new QuestionSaveRequestDto("title", "content"));
+
+        mockMvc.perform(get("/usr/doctor-qna/write"))
+                .andExpect(status().isOk())
+                .andExpect(view().name(viewName));
+
+    }
+
+    @Test
+    @WithMockUser(roles="MEMBER")
+    void saveTest_withValidRequestDto() throws Exception {
         // given
         QuestionSaveRequestDto requestDto = new QuestionSaveRequestDto("title", "content");
 
         // when
-        String viewName = questionController.save(requestDto, bindingResult, principal);
+        mockMvc.perform(post("/usr/doctor-qna/write")
+                        .param("title", requestDto.title())
+                        .param("content", requestDto.content()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/usr/doctor-qna"))
+                .andExpect(flash().attributeExists("message"));
 
         // then
-        assertThat(viewName).isEqualTo("redirect:/usr/doctor-qna");
-        verify(questionCommandService).save(requestDto, principal);
+        verify(questionCommandService, times(1)).save(requestDto, any(Principal.class));
     }
 
-    @DisplayName("QuestionController_질문등록_실패")
     @Test
-    void save_questionSaveRequestDtoIsInvalid_failure() throws Exception {
+    @WithMockUser(roles="MEMBER")
+    void saveTest_withInvalidRequestDto() throws Exception {
         // given
         QuestionSaveRequestDto requestDto = new QuestionSaveRequestDto("", "");
 
-        when(bindingResult.hasErrors()).thenReturn(true);
-
         // when
-        String viewName = questionController.save(requestDto, bindingResult, principal);
+        mockMvc.perform(post("/usr/doctor-qna/write")
+                        .param("title", requestDto.title())
+                        .param("content", requestDto.content()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("doctorqna/doctorQnaQuestionForm"));
 
         // then
-        assertThat(viewName).isEqualTo("doctorqna/doctorQnaQuestionForm");
         verifyNoInteractions(questionCommandService);
     }
 }
